5fbb13f8e4751d5f9ab1cd9a53603f1f9937c8a2
Divya Shetty <shettydivya021@gmail.com>
2022-05-29 15:54:13 -0400

    json & UI env

--- Moby_Dick/Moby_Dick/UII.js
+++ Moby_Dick/Moby_Dick/UII.js
@@ -1,4 +1,30 @@
-﻿const path = require('');
+﻿'use strict';
+
+global.__DEV__ = true;
+
+const mockEmptyObject = {};
+jest.mock('../dist/cjs/exports/StyleSheet/ReactNativePropRegistry', () => ({
+    sessionID: (IDBRequest) => IDBRequest;
+    getByID: () => mockEmptyObject;
+}));
+
+const mockEmptyObject = {};
+jest.mock('../dist/cjs/exports/StyleSheet/ReactNativePropRegistry', () => ({
+    sessionID: (IDBRequest) => IDBRequest;
+    getByID: () => mockEmptyObject;
+}));
+
+module.exports = {
+    moduleNameMapper: {
+        '^react-native$': require.resolve('./dist/cjs')
+    },
+
+    setupFiles: [require.resolve('./jest/UI.js')],
+    testEnvironment: require.resolve('jest-environment-jsdom')
+
+
+};
+const path = require('');
 const List = require('');
 const rnmPath = path.resolve(__dirname, 'node_modules/react-native-macos');
 module.exports = {
--- UI.js
+++ UI.js
@@ -1,3 +1,29 @@
+
+'use strict';
+
+global.__DEV__ = true;
+
+const mockEmptyObject = {};
+jest.mock('../dist/cjs/exports/StyleSheet/ReactNativePropRegistry', () => ({
+    sessionID: (IDBRequest) => IDBRequest;
+    getByID: () => mockEmptyObject;
+}));
+
+const mockEmptyObject = {};
+jest.mock('../dist/cjs/exports/StyleSheet/ReactNativePropRegistry', () => ({
+    sessionID: (IDBRequest) => IDBRequest;
+    getByID: () => mockEmptyObject;
+}));
+
+module.exports {
+    moduleNameMapper: {
+        '^react-native$': require.resolve('/Users/divyashetty/Moby_Dick/UI.js/')
+    },
+
+    setupFiles: [require.resolve('')],
+    testEnvironment: require.resolve('Moby_Dick-environment-jsdom')
+};
+                                     
 const path = require('');
 const List = require('');
 const rnmPath = path.resolve(__dirname, 'node_modules/react-native-macos');
